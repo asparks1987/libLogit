4a302ffae198f8578c51b56e1934bf0fa2514a3a
Aryn M Sparks <84827705+asparks1987@users.noreply.github.com>
2023-07-29 17:57:34 -0500

    installable wheel added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libLogit',
+    version='0.0.1a', 
+    url='https://github.com/asparks1987/libLogit.git',
+    author='Aryn M. Sparks',
+    author_email='Aryn.sparks1987@gmail.com',
+    description='A simple logging library in python',
+    packages=find_packages(),    
+    install_requires=[],
+)
